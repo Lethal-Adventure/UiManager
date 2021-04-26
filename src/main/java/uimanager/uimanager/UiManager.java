13f429c537e19a51db4ce8c4696f51143bcdcef7
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-04-26 12:11:26 -0400

    spiggot boiler

---
+++ src/main/java/uimanager/uimanager/UiManager.java
@@ -0,0 +1,17 @@
+package uimanager.uimanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UiManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
